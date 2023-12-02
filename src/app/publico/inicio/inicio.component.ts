56966c20af3dc562af12663dc128185047eb0bb0
SrJaniot <estebanjaniot10@gmail.com>
2023-12-01 22:57:45 -0500

    permisos por rol botones navbar, componente inicio con imagen

--- src/app/publico/inicio/inicio.component.ts
+++ src/app/publico/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+
+    constructor(
+      private router: Router,
+    ) { }
+
+
+    sesionActiva: boolean = false;
+    imagenUrl: string ="";
+    
+    // variable para mejorar la estetica del html
+    isHovering = false;
+
+
+    ngOnInit(): void {
+      this.validarsesionactiva();
+      if (this.sesionActiva) {
+        //console.log("sesion activa");
+        this.imagenUrl = localStorage.getItem('imagenUrl') || 'https://previews.123rf.com/images/newdesignillustrations/newdesignillustrations1902/newdesignillustrations190206672/125581696-prueba-el-texto-en-una-cinta-diseñado-con-título-blanco-y-franja-azul-banner-de-vector-con.jpg';
+
+      }else{
+        //volver a pagina login
+        this.router.navigate(['']);
+      }
+
+    }
+
+    validarsesionactiva() {
+      if (localStorage.getItem('datosUsuario-banproinpu')) {
+        this.sesionActiva = true;
+
+      }
+      else {
+        this.sesionActiva = false;
+      }
+    }
 
 }
--- src/app/publico/pagina-maestra/encabezado/encabezado.component.ts
+++ src/app/publico/pagina-maestra/encabezado/encabezado.component.ts
@@ -17,36 +17,105 @@ export class EncabezadoComponent {
       private toast: NgToastService,
     ) { }
     sesionActiva: boolean = false;
+    moduloparametros: boolean = false;
+    modulopersonas: boolean = false;
+    modulorecepcion: boolean = false;
+    moduloasignacion: boolean = false;
+    modulorevicion: boolean = false;
+    moduloregistro: boolean = false;
+    modulodevolucion: boolean = false;
+    modulocertificacion: boolean = false;
+    moduloconsultas: boolean = false;
+    moduloinformes: boolean = false;
+    moduloplandedesarrollo: boolean = false;
+    modulopresupuesto: boolean = false;
+
+    modulovalidacion: boolean = false; //pendiente por agregar en el backend (Jose)
+
+
 
     ngOnInit(): void {
-      this.ValidarSesionActiva();
+      this.validarModulos();
+      // console.log(this.sesionActiva);
+      //console.log(this.moduloparametros);
+      //console.log(this.modulopersonas);
+      //console.log(this.modulorecepcion);
+      //console.log(this.moduloasignacion);
+      //console.log(this.modulorevicion);
+      //console.log(this.moduloregistro);
+      //console.log(this.modulodevolucion);
+      //console.log(this.modulocertificacion);
+      //console.log(this.moduloconsultas);
+      //console.log(this.moduloinformes);
+      //console.log(this.moduloplandedesarrollo);
+      //console.log(this.modulopresupuesto);
+
 
     }
 
-    ValidarSesionActiva() {
+
+
+
+
+    validarModulos() {
       this.servicioSeguridad.ObteberDatosSesion().subscribe({
-
-
         next: (datos:SesionModel) => {
-
-
           if (datos.token != undefined) {
             this.sesionActiva = true;
-
-          }else{
+            if (datos.modulos) {
+              datos.modulos.forEach((modulo) => {
+                console.log(modulo.nombre);
+                switch (modulo.nombre) {
+                  case 'Parametros':
+                    this.moduloparametros = true;
+                    break;
+                  case 'Personas':
+                    this.modulopersonas = true;
+                    break;
+                  case 'Recepción':
+                    this.modulorecepcion = true;
+                    break;
+                  case 'Asignación':
+                    this.moduloasignacion = true;
+                    break;
+                  case 'Revisión':
+                    this.modulorevicion = true;
+                    break;
+                  case 'Registro':
+                    this.moduloregistro = true;
+                    break;
+                  case 'Devolución':
+                    this.modulodevolucion = true;
+                    break;
+                  case 'Certificación':
+                    this.modulocertificacion = true;
+                    break;
+                  case 'Consultas':
+                    this.moduloconsultas = true;
+                    break;
+                  case 'Informes':
+                    this.moduloinformes = true;
+                    break;
+                  case 'Plan de Desarrollo':
+                    this.moduloplandedesarrollo = true;
+                    break;
+                  case 'Presupuesto':
+                    this.modulopresupuesto = true;
+                    break;
+                  default:
+                    break;
+                }
+              });
+            }
+          } else {
             this.sesionActiva = false;
-
           }
-          //console.log(datos);
-
         },
         error: (error:any) => {
           //console.log(error);
           this.sesionActiva = false;
         }
       });
-
-
     }
 
 
